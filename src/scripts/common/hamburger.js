e572868992019011b0524cbcaa148a3c2e72bffa
Anna Vlasova <anja_kaa@mail.ru>
2017-09-14 13:59:46 +0300

    index header и footer  переделаны на BEMнейминг

--- src/scripts/common/hamburger.js
+++ src/scripts/common/hamburger.js
@@ -7,7 +7,7 @@ function hamburgerInit() {
   function toggleHandler(toggle) {
     toggle.addEventListener( "click", function(e) {
       e.preventDefault();
-      (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+      (this.classList.contains("hamburger_active") === true) ? this.classList.remove("hamburger_active") : this.classList.add("hamburger_active");
 
     });
   }
--- src/scripts/common/welcome.js
+++ src/scripts/common/welcome.js
@@ -1,10 +1,10 @@
 
 function welcomeInit() {
-	document.addEventListener('click',function(e) {
-		if(e.target.classList.contains('authorization_button')) {
-			document.querySelector('.authorization_button').style.display='none';
-			document.querySelector('.authorization').classList.remove('off');
+	document.addEventListener('click', function(e) {
+		if(e.target.classList.contains('authorization-button')) {
+			document.querySelector('.authorization-button').classList.add('off');
 			document.querySelector('.welcome').classList.add('off');
+			document.querySelector('.authorization').classList.remove('off');
 		}
 	})
 }
