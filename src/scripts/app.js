50c0435f99e86757547899345ec7ae4dc2a1a1d6
Anna Vlasova <anja_kaa@mail.ru>
2017-09-21 13:11:35 +0300

    добавлена стилисованая карта google

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,8 +2,10 @@ const welcome = require('./common/welcome');
 const blog = require('./common/blog');
 const hamburger = require('./common/hamburger');
 var arrowsButton = require('./common/arrows-buttons.js');
+var map = require('./common/google-map');
 
 welcome();
 hamburger();
 blog();
 arrowsButton();
+map();
---
+++ src/scripts/common/google-map.js
@@ -0,0 +1,221 @@
+function googleMapInit() {
+    // Местоположение: долгота, широта и коэффициент увеличения
+
+     var latitude = 55.769,
+         longitude = 37.677,
+         mapZoom = 13;
+        
+    // Стили для разных элементов на карте       
+    var style = [
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#61dac9"
+                },
+                {
+                    "lightness": 17
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 20
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#dedede"
+                },
+                {
+                    "lightness": 17
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#dedede"
+                },
+                {
+                    "lightness": 29
+                },
+                {
+                    "weight": 0.2
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#dedede"
+                },
+                {
+                    "lightness": 18
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f1f1f1"
+                },
+                {
+                    "lightness": 21
+                }
+            ]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "saturation": 36
+                },
+                {
+                    "color": "#333333"
+                },
+                {
+                    "lightness": 40
+                }
+            ]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f2f2f2"
+                },
+                {
+                    "lightness": 19
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#fefefe"
+                },
+                {
+                    "lightness": 20
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#fefefe"
+                },
+                {
+                    "lightness": 17
+                },
+                {
+                    "weight": 1.2
+                }
+            ]
+        }
+    ]
+
+    // Создание точки на карте
+    var mapOptions = {
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: mapZoom,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        styles: style
+    }
+
+    // Инициализация карты
+    var map = new google.maps.Map(document.getElementById('google-container'), mapOptions);
+
+    // Добавляем свой маркер местонахождения на карту (свою иконку)			
+    // var marker = new google.maps.Marker({
+    //     position: new google.maps.LatLng(latitude, longitude),
+    //     map: map,
+    //     visible: true,
+    //     icon: marker_url,
+    // });
+
+    //Добавляем свои иконки для кнопок увеличить/уменьшить на карту
+    function CustomZoomControl(controlDiv, map) { 
+        var controlUIzoomIn= document.getElementById('zoom-in'),
+            controlUIzoomOut= document.getElementById('zoom-out');
+            controlDiv.appendChild(controlUIzoomIn);
+            controlDiv.appendChild(controlUIzoomOut);
+    
+            //Делаем привязку для кнопок увеличить/уменьшить при клике
+            google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
+                map.setZoom(map.getZoom()+1)
+            });
+            google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
+                 map.setZoom(map.getZoom()-1)
+            });
+    }
+
+    var zoomControlDiv = document.createElement('div');
+    var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+
+    //Помещаем кнопки увеличить/уменьшить на карту в левый верхний угол
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+}
+
+module.exports = googleMapInit;
