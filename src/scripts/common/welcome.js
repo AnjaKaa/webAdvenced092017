2e55f362ecc953bceb9e5d85aeaf6f93fd87a75e
Anna Vlasova <anja_kaa@mail.ru>
2017-09-22 16:15:25 +0300

    flip-эффект на главной странице

--- src/scripts/common/welcome.js
+++ src/scripts/common/welcome.js
@@ -1,10 +1,17 @@
 
 function welcomeInit() {
 	document.addEventListener('click', function(e) {
-		if(e.target.classList.contains('authorization-button')) {
+		if (e.target.classList.contains('authorization-button')) {
 			document.querySelector('.authorization-button').classList.add('off');
-			document.querySelector('.welcome').classList.add('off');
-			document.querySelector('.authorization').classList.remove('off');
+			document.querySelector('.center-panel__front').classList.add('off');
+			document.querySelector('.center-panel__back').classList.remove('off');
+			document.querySelector('.center-panel').classList.add('center-panel--rotate');
+		}
+		if (e.target.classList.contains('to_welcome') || e.target.classList.contains('index__wrap')) {
+			document.querySelector('.authorization-button').classList.remove('off');
+			document.querySelector('.center-panel__front').classList.remove('off');
+			document.querySelector('.center-panel__back').classList.add('off');
+			document.querySelector('.center-panel').classList.remove('center-panel--rotate');
 		}
 	})
 }
